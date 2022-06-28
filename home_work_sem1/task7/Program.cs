23bc35f3a7ebcd1b1867951290191abc1f9b29e7
mich <dilema82p@gmail.com>
2022-06-28 22:16:24 +0300

    решение задачи 7

---
+++ home_work_sem1/task7/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите трехзначное число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+int Third_Digit=0;
+double Buffer = Num;
+while (Buffer>1)
+   {
+    Buffer=Buffer/10;
+    Third_Digit++;
+   }
+if (Third_Digit == 3)
+   {
+    int Result = Num%10;
+    Console.WriteLine("Последняя цифра числа " + Num + " - " +Result);
+   }
+else
+   {
+    Console.WriteLine("Аяяяй! "+ Num + " нетрёхзначное число!" );
+   }
