e7994222969bab447990f53a472d44f3516635fe
mich <dilema82p@gmail.com>
2022-06-28 20:40:25 +0300

    решение задачи 2

---
+++ home_work_sem1/task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+        Console.WriteLine("А вот и ответ: ");
+        Console.Write("{a} = max, {b} = min");
+}
+
+else  
+{
+        Console.WriteLine("А вот и ответ: ");
+        Console.Write("b = max, a = min");
+}
