5c8a2c3c21f8ea205e736ce2d7d282f7ce505c42
mich <dilema82p@gmail.com>
2022-06-30 13:43:07 +0300

    Задача 10. Вывод второй цифры трёхзначного числа

---
+++ home_work_sem2/task10/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 10. Программа, 
+которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+*/
+
+Console.Write("Введите трехзначное число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+int Second_Digit = 0;
+double Buffer = Num;
+while (Buffer > 1)
+   {
+    Buffer = Buffer / 10;
+    Second_Digit++;
+   }
+if (Second_Digit == 3)
+   {
+    string stringNumber = Convert.ToString(Num);
+    Console.WriteLine("вторая цифра этого числа: "+stringNumber[1]);
+   }
+else
+   {
+    Console.WriteLine("Аяяяй! "+ Num + " нетрёхзначное число!" );
+   }
