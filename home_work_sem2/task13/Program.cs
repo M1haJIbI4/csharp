27513eb528e6b79e2232e6680e0f539dde96e243
mich <dilema82p@gmail.com>
2022-06-30 14:05:48 +0300

    Задача 13. Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.

---
+++ home_work_sem2/task13/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задача 13. Программа, которая выводит третью цифру
+заданного числа или сообщает, что третьей цифры нет.*/
+
+
+Console.Write("Введите число: "); 
+int randomNumber = Convert.ToInt32(Console.ReadLine()); 
+string randomNumberText = Convert.ToString(randomNumber); 
+if (randomNumberText.Length > 2)
+        {
+                 Console.WriteLine("третья цифра введенного числа: "+randomNumberText[2]);
+                  } 
+else 
+{ Console.WriteLine("третьей цифры нет"); }
