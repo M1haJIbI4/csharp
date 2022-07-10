232375cb667da3da72f7af3a245cd50fc23d90da
mich <dilema82p@gmail.com>
2022-07-10 23:12:40 +0300

    решил задачу 25

---
+++ homework_sem4/task25/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+и возводит число A в натуральную степень B.*/
+int taskExp (int numA, int numB)
+{
+  int res = 1;
+  for (int i=1; i <= numB; i++)
+  {
+    res = res * numA;
+  }
+    // int res = Math.Pow(numA, numB);
+    return res;
+}
+
+  Console.Write("Введите число A: ");
+  int numA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int numB = Convert.ToInt32(Console.ReadLine());
+  if (numA > 0 && numB > 0 )
+    {
+     int taskexp = taskExp(numA, numB);
+     Console.WriteLine("Ответ: " + taskexp);
+    }
+  else
+    {
+     Console.WriteLine($"Внимание! Числа {numA} и/или {numB} не является натуральным, введите число больше 0");
+     return;
+    }
+  
