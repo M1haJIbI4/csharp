fec1e2423dae57d432185e0af0e21be4259ab4a8
mich <dilema82p@gmail.com>
2022-07-10 23:53:45 +0300

    решил задачу 29

---
+++ homework_sem4/task29/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов, 
+заполненный псевдослучайными числами и выводит их на экран.*/
+int [] elements = new int[8];
+Console.Write("[");
+for (int i = 0; i < elements.Length; i++)
+ {
+    elements [i] = new Random().Next(0, 11);
+    Console.Write(" " + Meth (i) + " ");
+ }
+Console.Write("]");
+int Meth (int a)
+{
+    return elements[a];
+}
