b3f9baa464e4cfb6be493d9c8192eb6116c26019
mich <dilema82p@gmail.com>
2022-07-21 00:51:55 +0300

    решил задачу 47, m n

---
+++ homework_sem7/task47/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.*/
+
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"m = {m}, n = {n}.");
+double[,] arr = new double[m, n];
+CreateArrDouble(arr);
+WriteArr(arr);
+Console.WriteLine();
+void CreateArrDouble(double[,] arr)
+{
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      arr[i, j] = new Random().NextDouble() * 20 - 10;
+    }
+  }
+}
+void WriteArr (double[,] arr)
+{
+for (int i = 0; i < m; i++)
+  {
+      for (int j = 0; j < n; j++)
+      {
+        double alignNumber = Math.Round(arr[i, j], 1);
+        Console.Write(alignNumber + "| ");
+      }
+      Console.WriteLine();
+  }
+}
