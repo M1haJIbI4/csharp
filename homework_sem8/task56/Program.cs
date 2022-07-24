5405bef3f27fb6305b42da120e38d3c912d4cb43
mich <dilema82p@gmail.com>
2022-07-25 00:14:42 +0300

    Решение задачи 56

---
+++ homework_sem8/task56/Program.cs
@@ -0,0 +1,60 @@
+﻿/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+которая будет находить строку с наименьшей суммой элементов.*/
+Console.WriteLine($"Задайте массив.");
+int m = InputNum("Введите количество строк: ");
+int n = InputNum("Введите количество столбцов: ");
+int range = InputNum("Введите диапазон строк: от 1 до ");
+int[,] arr = new int[m, n];
+CreateArr(arr);
+WriteArr(arr);
+int minSumLine = 0;
+int sumLine = SumLineElements(arr, 0);
+for (int i = 1; i < arr.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(arr, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+Console.WriteLine($"{minSumLine+1} - эта строкa с наименьшей суммой элементов - {sumLine}");
+int SumLineElements(int[,] arr, int i)
+{
+  int sumLine = arr[i,0];
+  for (int j = 1; j < arr.GetLength(1); j++)
+  {
+    sumLine += arr[i,j];
+  }
+  return sumLine;
+}
+
+int InputNum(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArr(int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      arr[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArr (int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      Console.Write(arr[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
