17a01abaa4b9d002e7b3d118a50a9d51b72cc958
mich <dilema82p@gmail.com>
2022-07-25 01:32:01 +0300

    решел задачу 62

---
+++ homework_sem8/task62/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 62: Заполните спирально массив 4 на 4.
+int n = 04;
+int[,] sqareMatrix = new int[n, n];
+int temp = 01;
+int i = 00;
+int j = 00;
+while (temp <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
+{
+  sqareMatrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < sqareMatrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= sqareMatrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > sqareMatrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+WriteArr(sqareMatrix);
+void WriteArr (int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      if (arr[i,j] / 10 <= 0)
+      Console.Write($" {arr[i,j]} ");
+
+      else Console.Write($"{arr[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
